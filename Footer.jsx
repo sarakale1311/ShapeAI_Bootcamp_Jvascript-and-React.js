c2cb076ec8158821b4148265399a56ffc6aa18a8
sarakale1311 <82115014+sarakale1311@users.noreply.github.com>
2021-06-07 11:45:44 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var currYear =new Date().getFullYear();
+  return (
+    <Footer>
+      <p>copyright @ {currYear}</p>
+      </Footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and Reacr.js</h1>
+      <p>
+        {" "}
+        This was an amazing bootcamp taken by Shaurya Sinha we covered
+        everything from scratch including Javascript,React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
